37305b84a568261675dd2650f3c508febdcd46c1
Henrique Carvalho de Souza <niguiealmeida@hotmail.com>
2021-08-09 12:14:34 -0300

    projeto CRUD Node Js

---
+++ src/servidor.js
@@ -0,0 +1,46 @@
+const porta = 3003
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+//metodo depreciado
+app.use(bodyParser.urlencoded({ extended:true }))
+
+//metodo atual
+//app.use(express.urlencoded({ extended:true })
+
+app.get('/produtos', (req, res, next) =>{
+    res.send(bancoDeDados.getProdutos()) //Converter para JSON     
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)// GERA UM JSON 
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)// GERA UM JSON 
+})
+
+app.delete('/produtos/:id', (req, res, next ) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto) //JSON
+}) 
+
+app.listen(porta, () => {
+    console.log(`Servidor está executando na porta ${porta}.`)
+})
